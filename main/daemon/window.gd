e930f203707c156ece2ae7bb95dbdfa388724875
quinnyo <3379314+quinnyo@users.noreply.github.com>
2022-10-09 02:40:23 +1000

    Godot 4 Engine.target_fps -> max_fps

--- main/daemon/window.gd
+++ main/daemon/window.gd
@@ -2,13 +2,11 @@
 ## Manage game window
 extends Node
 
-const SETT_TARGET_FPS := "quinn/display/target_fps"
-const SETT_UNFOCUSED_TARGET_FPS := "quinn/display/unfocused_target_fps"
+const PROJECT_MAX_FPS := "application/run/max_fps"
+const PROJECT_MAX_FPS_UNFOCUSED := "quinn/display/max_fps_unfocused"
 
 
 func _ready() -> void:
-	Engine.target_fps = get_target_fps()
-
 	# Correct size & floating state -- workaround for some tiling window managers.
 	# Make window unresizable in ProjectSettings, allow resizing once window created:
 	get_tree().root.unresizable = false
@@ -17,22 +15,22 @@ func _ready() -> void:
 func _notification(what: int) -> void:
 	match what:
 		NOTIFICATION_APPLICATION_FOCUS_IN:
-			Engine.target_fps = get_target_fps()
+			Engine.max_fps = get_max_fps()
 		NOTIFICATION_APPLICATION_FOCUS_OUT:
-			var fps := get_unfocused_target_fps()
+			var fps := get_max_fps_unfocused()
 			if fps > 0:
-				Engine.target_fps = fps
+				Engine.max_fps = fps
 
 
-func get_target_fps() -> int:
-	if ProjectSettings.has_setting(SETT_TARGET_FPS):
-		return ProjectSettings.get_setting(SETT_TARGET_FPS)
+func get_max_fps() -> int:
+	if ProjectSettings.has_setting(PROJECT_MAX_FPS):
+		return ProjectSettings.get_setting(PROJECT_MAX_FPS)
 	else:
 		return 0
 
 
-func get_unfocused_target_fps() -> int:
-	if ProjectSettings.has_setting(SETT_UNFOCUSED_TARGET_FPS):
-		return ProjectSettings.get_setting(SETT_UNFOCUSED_TARGET_FPS)
+func get_max_fps_unfocused() -> int:
+	if ProjectSettings.has_setting(PROJECT_MAX_FPS_UNFOCUSED):
+		return ProjectSettings.get_setting(PROJECT_MAX_FPS_UNFOCUSED)
 	else:
 		return 0
